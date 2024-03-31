b7d715c450ea03a201cd2a5fc4c164b97c8b753d
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 20:46:12 -0400

    Integer | Constant Integer additions

--- Integer-benchmark/integer-benchmark.cpp
+++ Integer-benchmark/integer-benchmark.cpp
@@ -4,3 +4,11 @@
 using namespace std;
 using namespace std::chrono;
 
+void Calculations(){
+    int result; // Stores result of the current calculation
+    
+    for (int i = 0; i < 1000000000; i++){ // Loop for the 10^9 iterations
+        result = 1 + 1; // Addition of integer constants
+    }
+    
+}
