16ace407a2aae6cb46f92e7ccb86083ee96558c2
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 20:50:25 -0400

    Integer | Multiplication & Division calculations

--- Integer-benchmark/integer-benchmark.cpp
+++ Integer-benchmark/integer-benchmark.cpp
@@ -7,8 +7,18 @@ using namespace std::chrono;
 void Calculations(){
     int result; // Stores result of the current calculation
     
-    for (int i = 0; i < 1000000000; i++){ // Loop for the 10^9 iterations
+    // Addition
+    for (int i = 0; i < 1000000000; i++){ // Loop for the 10^10 iterations
         result = 1 + 1; // Addition of integer constants
     }
     
+    //Multiplication
+    for (int i = 0; i < 5000000000; i++){ // Loop for the 5 * 10^9 iterations
+        result = 1 * 1; // Multiplication of integer constants
+    }
+
+    //Division
+    for (int i = 0; i < 2000000000; i++){ // Loop for the 2 * 10^9 iterations
+        result = 1 / 1; // Multiplication of integer constants
+    }
 }
