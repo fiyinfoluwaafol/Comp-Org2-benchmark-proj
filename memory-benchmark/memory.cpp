d15002b355c07baf52afcf0a5c7fe1bf32f427ec
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 22:06:44 -0400

    Memory | Writing & Reading from Array

--- memory-benchmark/memory.cpp
+++ memory-benchmark/memory.cpp
@@ -5,3 +5,16 @@ using namespace std;
 using namespace std::chrono;
 
 #define ARRAY_SIZE 5000000000
+
+void MemoryBenchmark(){
+    int nums[ARRAY_SIZE];
+    int num;
+
+    for (long long int i = 0; i < ARRAY_SIZE; i++){
+        nums[i] = i;
+    }
+
+    for (long long int i = 0; i < ARRAY_SIZE; i++){
+        num = nums[i];
+    }
+}
