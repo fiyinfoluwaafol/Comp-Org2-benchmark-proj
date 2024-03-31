65bf4d74bdfe91d19defac45a03e65848819a371
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 21:17:28 -0400

    Integer | Formatted the time and printed it

--- Integer-benchmark/integer-benchmark.cpp
+++ Integer-benchmark/integer-benchmark.cpp
@@ -8,17 +8,17 @@ void Calculations(){
     int result; // Stores result of the current calculation
     
     // Addition
-    for (int i = 0; i < 1000000000; i++){ // Loop for the 10^10 iterations
+    for (long long i = 0; i < 1000000000; i++){ // Loop for the 10^10 iterations
         result = 1 + 1; // Addition of integer constants
     }
     
-    //Multiplication
-    for (int i = 0; i < 5000000000; i++){ // Loop for the 5 * 10^9 iterations
+    //Multiplication 5000000000
+    for (long long i = 0; i < 2000000000; i++){ // Loop for the 5 * 10^9 iterations
         result = 1 * 1; // Multiplication of integer constants
     }
 
-    //Division
-    for (int i = 0; i < 2000000000; i++){ // Loop for the 2 * 10^9 iterations
+    //Division 2000000000
+    for (long long i = 0; i < 5000000000; i++){ // Loop for the 2 * 10^9 iterations
         result = 1 / 1; // Multiplication of integer constants
     }
 }
@@ -28,6 +28,11 @@ int main(){
     auto start = high_resolution_clock::now();
     Calculations();
     auto stop = high_resolution_clock::now();
+
+    auto runTime = duration_cast<milliseconds>(stop-start);  
     
-    auto runTime = duration_cast<milliseconds>(stop-start);    
+    double seconds = runTime.count() / 1000.0;
+    int minutes = seconds / 60;
+
+    cout << minutes << " m " << std::fmod(seconds, 60) << " s\n";
 }
