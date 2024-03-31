20651174b7be246ad876aaf073703fc74171ee2d
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 21:42:51 -0400

    Floating Point | Constant float additions

--- floating-point-benchmark/float-point-benchmark.cpp
+++ floating-point-benchmark/float-point-benchmark.cpp
@@ -3,3 +3,12 @@
 #include <iostream>
 using namespace std;
 using namespace std::chrono;
+
+void Calculations(){
+    float result; // Stores result of the current calculation
+    
+    // Addition
+    for (long long i = 0; i < 1000000000; i++){ // Loop for the 10^10 iterations
+        result = 1.5 + 1.7; // Addition of integer constants
+    }
+}
