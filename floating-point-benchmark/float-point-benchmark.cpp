1ad0df0df6b57dff400f54ce1c3d064c284e7206
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 21:44:07 -0400

    Floating Point | Multiplication & Division calculations

--- floating-point-benchmark/float-point-benchmark.cpp
+++ floating-point-benchmark/float-point-benchmark.cpp
@@ -11,4 +11,14 @@ void Calculations(){
     for (long long i = 0; i < 1000000000; i++){ // Loop for the 10^10 iterations
         result = 1.5 + 1.7; // Addition of integer constants
     }
+
+     //Multiplication 5000000000
+    for (long long i = 0; i < 2000000000; i++){ // Loop for the 5 * 10^9 iterations
+        result = 1.5 * 1.7; // Multiplication of integer constants
+    }
+
+    //Division 2000000000
+    for (long long i = 0; i < 5000000000; i++){ // Loop for the 2 * 10^9 iterations
+        result = 1.5 / 1.7; // Multiplication of integer constants
+    }
 }
