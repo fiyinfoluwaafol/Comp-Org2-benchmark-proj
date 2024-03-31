3fc2230db42f9eaa04f30582ebfa45f10d01ac80
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-03-30 22:19:36 -0400

    Memory | Created Main for timing the benchmark

--- memory-benchmark/memory.cpp
+++ memory-benchmark/memory.cpp
@@ -7,14 +7,28 @@ using namespace std::chrono;
 #define ARRAY_SIZE 5000000000
 
 void MemoryBenchmark(){
-    int nums[ARRAY_SIZE];
+    int *nums = new int[ARRAY_SIZE];
     int num;
 
     for (long long int i = 0; i < ARRAY_SIZE; i++){
-        nums[i] = i;
+        num = *(nums + i);
     }
 
     for (long long int i = 0; i < ARRAY_SIZE; i++){
-        num = nums[i];
+        *(nums + i) = 1;
     }
+}
+
+int main(){
+
+    auto start = high_resolution_clock::now();
+    MemoryBenchmark();
+    auto stop = high_resolution_clock::now();
+
+    auto runTime = duration_cast<milliseconds>(stop-start);  
+    
+    double seconds = runTime.count() / 1000.0;
+    int minutes = seconds / 60;
+
+    cout << minutes << " m " << std::fmod(seconds, 60) << " s\n";
 }
